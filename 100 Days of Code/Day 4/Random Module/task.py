f13e735685a30cc1e61fa049bd1710a7ae2dea87
madankumar6 <madankumar6@gmail.com>
2024-11-25 23:13:56 +0530

    Added code for day 4

---
+++ 100 Days of Code/Day 4/Random Module/task.py
@@ -0,0 +1,11 @@
+import random
+import my_module
+
+random_number = random.random()
+print(random_number)
+
+zero_one = random.randint(0, 1)
+print(zero_one)
+
+print(f"My favorite number is {my_module.my_favorite_number}")
+
